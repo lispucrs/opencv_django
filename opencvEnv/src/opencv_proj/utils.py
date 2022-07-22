7ab3ff9697af4951b67e9bd780054ec181f5df3e
Felipe <zfelipesilveira@gmail.com>
2022-07-22 14:23:39 -0300

    anotações

--- opencvEnv/src/opencv_proj/utils.py
+++ opencvEnv/src/opencv_proj/utils.py
@@ -5,14 +5,17 @@ import numpy as np
 
 from .noiser import Noiser
 
-ns = Noiser(None)
+#ns = Noiser(None)
 
 
 def get_filtered_image(image, action):
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     filtered = None
     if action == 'NO_FILTER':
-        filtered = image
+        #filtered = image
+        ns = Noiser(None)
+        ns.change_image(img)
+        filtered = ns.gradient_gaussian()
 
     elif action == 'COLORIZED':
         filtered = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
